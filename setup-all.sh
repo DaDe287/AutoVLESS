2db5cd0e32154bf198d0fe7f90f5f6c7779910c8
Anatoli <64985361+DaDe287@users.noreply.github.com>
2025-08-27 22:31:40 +0300

    Create setup-all.sh

---
+++ setup-all.sh
@@ -0,0 +1,8 @@
+clear
+echo "y" | sudo apt update && apt upgrade -y
+
+bash <(curl -Ls https://raw.githubusercontent.com/DaDe287/AutoVLESS/main/auto-panel-ssl.sh)
+
+bash <(curl -Ls https://raw.githubusercontent.com/DaDe287/AutoVLESS/main/install-panel-and-xray.sh)
+
+bash <(curl -Ls https://raw.githubusercontent.com/DaDe287/AutoVLESS/main/setup-firewall.sh)
