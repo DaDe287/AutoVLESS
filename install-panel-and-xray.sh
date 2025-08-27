b7bc25b0c57bee7546acaa0d6486f8bb0778023a
Anatoli <64985361+DaDe287@users.noreply.github.com>
2025-08-27 22:27:05 +0300

    Update install-panel-and-xray.sh

--- install-panel-and-xray.sh
+++ install-panel-and-xray.sh
@@ -93,7 +93,7 @@ config_after_install() {
             local config_webBasePath=$(gen_random_string 18)
             local config_username="admin"
             local config_password=$(gen_random_string 10)
-            local config_port=443
+            local config_port="443"
 
             /usr/local/x-ui/x-ui setting -username "${config_username}" -password "${config_password}" -port "${config_port}" -webBasePath "${config_webBasePath}"
             echo -e "This is a fresh installation, generating random login info for security concerns:"
