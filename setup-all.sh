9b01297b3caec1d8abd50956ca055c5564d38ee6
Anatoli <64985361+DaDe287@users.noreply.github.com>
2025-09-17 16:07:23 +0300

    Update setup-all.sh

--- setup-all.sh
+++ setup-all.sh
@@ -1,11 +1,11 @@
 clear
 echo "y" | sudo apt update && apt upgrade -y
 
-bash <(curl -Ls https://raw.githubusercontent.com/DaDe287/AutoVLESS/main/setup-firewall.sh)
-
 bash <(curl -Ls https://raw.githubusercontent.com/DaDe287/AutoVLESS/main/install-panel-and-xray.sh)
 
 bash <(curl -Ls https://raw.githubusercontent.com/DaDe287/AutoVLESS/main/auto-panel-ssl.sh)
+
+bash <(curl -Ls https://raw.githubusercontent.com/DaDe287/AutoVLESS/main/setup-firewall.sh)
 
 echo "$13" | x-ui 
 
