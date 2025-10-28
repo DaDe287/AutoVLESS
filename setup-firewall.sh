e2e91618f7eba16df7eb9db3b02fe23b02a98179
Anatoli <64985361+DaDe287@users.noreply.github.com>
2025-10-29 01:35:21 +0300

    Update setup-firewall.sh

--- setup-firewall.sh
+++ setup-firewall.sh
@@ -1,7 +1,7 @@
 sudo ufw allow 443/tcp
 sudo ufw allow 8080/tcp
 sudo ufw allow 22/tcp
+sudo ufw allow 80/tcp
 sudo ufw allow 1024:65535/udp
 sudo ufw allow 1024:65535/tcp
-sudo ufw allow 80/tcp
 echo "y" | sudo ufw enable
