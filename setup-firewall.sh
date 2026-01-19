5ff5c3029a721dc947d8f383eff1bdbb2c21b002
Anatoli <64985361+DaDe287@users.noreply.github.com>
2026-01-19 17:15:03 +0300

    Update setup-firewall.sh

--- setup-firewall.sh
+++ setup-firewall.sh
@@ -1,3 +1,6 @@
+# UFW
+
+# COMMON FIREWALL RULES
 echo "Y" | sudo apt install ufw
 sudo ufw allow 443/tcp
 sudo ufw allow 8080/tcp
@@ -6,3 +9,10 @@ sudo ufw allow 80/tcp
 sudo ufw allow 1024:65535/udp
 sudo ufw allow 1024:65535/tcp
 echo "y" | sudo ufw enable
+
+# IPTABLES 
+
+# Block Torrents trafic
+echo "Y" | sudo apt install iptables
+iptables -A FORWARD -p tcp --dport 6881:6999 -j DROP
+iptables -A FORWARD -p udp --dport 6881:6999 -j DROP
