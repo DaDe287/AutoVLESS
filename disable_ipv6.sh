af99afdc75d746c95a551b1a0cdd2bc952a5e3ae
Anatoli <64985361+DaDe287@users.noreply.github.com>
2026-02-12 19:25:47 +0300

    Update disable_ipv6.sh

--- disable_ipv6.sh
+++ disable_ipv6.sh
@@ -3,6 +3,11 @@ sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
 sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
 sudo sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 
+# Disable Ipv6
+echo "net.ipv6.conf.all.disable_ipv6 = 1" > /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" > /etc/sysctl.conf
+echo "net.ipv6.conf.lo.disable_ipv6 = 1" > /etc/sysctl.conf
+
 # Commit changes and reboot
 sudo sysctl -p
 sudo reboot
