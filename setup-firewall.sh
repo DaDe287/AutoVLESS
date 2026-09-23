990498cac03dcd59956666a83034e009719b232c
Anatoli <64985361+DaDe287@users.noreply.github.com>
2026-09-23 03:54:56 +0300

    Update setup-firewall.sh

--- setup-firewall.sh
+++ setup-firewall.sh
@@ -1,5 +1,5 @@
 # UFW
-# Common Firewall rules
+## Common Firewall rules
 echo "Y" | sudo apt install ufw
 
 ufw --force reset
@@ -9,11 +9,11 @@ for p in 22 80 443 8443 5506 2096; do ufw allow $p/tcp; done
 ufw --force enable
 
 # IPTABLES 
-# Block Torrents trafic
+## Block Torrents trafic
 echo "Y" | sudo apt install iptables
 iptables -A FORWARD -p tcp --dport 6881:6999 -j DROP
 iptables -A FORWARD -p udp --dport 6881:6999 -j DROP
 
 # Trafic-Guard
-# Block Any scanners to the server
+## Block Any scanners to the server
 curl -fsSL https://raw.githubusercontent.com/DaDe287/Traffic-Guard/refs/heads/main/install.sh | sudo bash
